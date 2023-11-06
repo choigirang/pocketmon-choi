2ce4bba23ca72f6cd5fdebd520b02456812ec791
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-11-06 16:27:00 +0900

    Update index.tsx

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { styled } from "styled-components";
+import Window from "@/components/desktop/Window";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Logo>pocketmon</Logo>
+      <Window></Window>
     </>
   );
 }
