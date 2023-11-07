76ec5d686d2c80bfaa38df44744d1983426e7c7c
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-11-07 22:02:33 +0900

    feat: 클릭, 더블클릭 이벤트 분리 및 더블 클릭 시 해당하는 창 오픈

--- front/src/components/desktop/IconGrid.tsx
+++ front/src/components/desktop/IconGrid.tsx
@@ -1,25 +1,67 @@
-import React from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 import { styled } from "styled-components";
 import Image from "next/image";
 
-const imageNames = ["computer.png"];
+const imageNames = ["My-Computer.png"];
 
-export default function IconGrid() {
+export default function IconGrid({
+  changeBg,
+  setClickedItem,
+  setChangeBg,
+}: {
+  changeBg: undefined | number;
+  setClickedItem: Dispatch<SetStateAction<undefined | string>>;
+  setChangeBg: Dispatch<SetStateAction<undefined | number>>;
+}) {
+  // 클릭과 더블 클릭 구분을 위한
+  const [clicked, setClicked] = useState(false);
+
+  // 확장자 지우기
   function removeExtension(fileName: string) {
     return fileName.split(".").slice(0, -1).join(".");
+  }
+
+  // 클릭한 요소에 따른 bg 부여
+  function changeClick(index: number) {
+    if (!clicked) {
+      if (changeBg === index) return setChangeBg(undefined);
+      setClicked(true);
+      setTimeout(() => {
+        // 지정한 시간(예: 300ms) 후 클릭 상태를 초기화
+        setClicked(false);
+      }, 300);
+      setChangeBg(index);
+    }
+  }
+
+  // 더블 클릭한 요소를 WindowWallPaper에서 folder로 띄우기
+  function loadFolder(imgName: string) {
+    setClicked(false);
+    setClickedItem(removeExtension(imgName).replaceAll(/-/g, ``));
+    const width = 800; // 새 창의 가로 크기
+    const height = 600; // 새 창의 세로 크기
+    window.open("/computer", "", `width=${width},height=${height}`);
   }
 
   return (
     <Container>
       <ItemBox>
         {imageNames.map((imageName, index) => (
-          <Item key={index}>
+          <Item
+            key={index}
+            onClick={() => changeClick(index)}
+            onDoubleClick={() => loadFolder(imageName)}
+          >
+            {/* 클릭 시 blue bg 추가 */}
+            {changeBg === index && <ClickChangeBg />}
             <Image
               src={require(`../../../public/image/${imageName}`)}
               alt={imageName}
               className="icon"
             />
-            <span>{removeExtension(imageName)}</span>
+            <NameTag>
+              {removeExtension(imageName).replaceAll(/-/g, ` `)}
+            </NameTag>
           </Item>
         ))}
       </ItemBox>
@@ -28,7 +70,6 @@ export default function IconGrid() {
 }
 
 const Container = styled.div`
-  width: 100%;
   height: 100%;
   padding: 10px;
   display: flex;
@@ -43,14 +84,30 @@ const ItemBox = styled.ul`
 `;
 
 const Item = styled.li`
+  padding: 5px;
+  position: relative;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   gap: 7px;
+  cursor: pointer;
 
   .icon {
     width: 50px;
     height: auto;
   }
+`;
+
+const NameTag = styled.span`
+  color: white;
+  z-index: 1;
+`;
+
+const ClickChangeBg = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(38, 42, 245, 0.5);
+  border: dashed 1px white;
 `;
