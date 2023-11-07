c990a1e03d1b7762f733f4368b19c19bf100ff9e
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-11-07 22:03:09 +0900

    feat: dynamic으로 더블 클릭한 컴포넌트 open

--- front/src/components/desktop/WindowWallPaper.tsx
+++ front/src/components/desktop/WindowWallPaper.tsx
@@ -1,16 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Image from "next/image";
 import logo from "../../../public/image/Windows_Logo.svg";
-import BtmNav from "./BtmNav";
+import BtmNav from "./bottom/BtmNav";
 import IconGrid from "./IconGrid";
+import dynamic from "next/dynamic";
+
+function folder(clickedItem: string | undefined) {
+  if (!clickedItem) return null;
+  const FolderComponent = dynamic(() => import(`../folder/${clickedItem}`), {
+    loading: () => <div>Loading...</div>, // 로딩 중에 보일 컴포넌트
+    ssr: false, // 서버사이드 렌더링 비활성화
+  });
+  return <FolderComponent />;
+}
 
 export default function WindowWallPaper() {
+  const [renderComponent, setRenderComponent] = useState<JSX.Element | null>(
+    null
+  );
+
+  // 클릭한 요소 배경 변경
+  const [changeBg, setChangeBg] = useState<undefined | number>(undefined);
+
+  // handle click icon
+  const [clickedItem, setClickedItem] = useState<undefined | string>(undefined);
+
+  // handle menu barw
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    setRenderComponent(folder(clickedItem));
+  }, [clickedItem]);
+
   return (
     <Container>
-      <IconGrid />
-      <BtmNav />
-      <Bg>
+      {/* 폴더 */}
+      {renderComponent}
+      {/* 아이콘 모음 */}
+      <IconGrid
+        setClickedItem={setClickedItem}
+        changeBg={changeBg}
+        setChangeBg={setChangeBg}
+      />
+      {/* 하단바 */}
+      <BtmNav menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      {/* 배경 */}
+      <Bg
+        onClick={() => {
+          setChangeBg(undefined);
+          setClickedItem(undefined);
+        }}
+      >
         <Image src={logo} alt="bg-logo" className="logo" />
       </Bg>
     </Container>
