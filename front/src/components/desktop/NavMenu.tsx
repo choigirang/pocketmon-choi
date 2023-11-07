c7395fcf862f6cf86429baa4dc8d3a2872598e2c
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-11-07 15:57:34 +0900

    feat: start btn 클릭 시 나타날 메뉴

---
+++ front/src/components/desktop/NavMenu.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { styled } from "styled-components";
+
+export default function NavMenu() {
+  return <Container></Container>;
+}
+
+const Container = styled.ul`
+  width: 150px;
+  height: 100px;
+  background-color: white;
+  position: absolute;
+  bottom: 60px;
+`;
