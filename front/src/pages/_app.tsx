40950dd79e3565259ba548da447d2cf4d7ede95f
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-11-08 09:56:26 +0900

    feat: css 변수명 사용을 위한 common css import

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import "@/styles/index.css";
+import "@/styles/common.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
