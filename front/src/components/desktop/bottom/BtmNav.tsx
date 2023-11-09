d90da1a0eb96d285f37a2a91f044f3c337105679
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-11-09 12:40:57 +0900

    feat: 하단 메뉴바 시계 추가

--- front/src/components/desktop/bottom/BtmNav.tsx
+++ front/src/components/desktop/bottom/BtmNav.tsx
@@ -1,9 +1,10 @@
-import React, { SetStateAction, useState } from "react";
+import React, { SetStateAction, useEffect, useState } from "react";
 import { styled } from "styled-components";
 import Image from "next/image";
 import pixel_logo from "img/window_pixel.svg";
 import { BtmOpenProps } from "@/types/props";
 import NavMenu from "./NavMenu";
+import clock from "img/clock.svg";
 
 export default function BtmNav({
   menuOpen,
@@ -12,6 +13,30 @@ export default function BtmNav({
   menuOpen: boolean;
   setMenuOpen: React.Dispatch<SetStateAction<boolean>>;
 }) {
+  // 시간 설정 Clock
+  const [timer, setTimer] = useState("00:00");
+
+  // useEffect 없이 함수만으로 setInterval
+  // 지정했으나 too many rerender err
+  useEffect(() => {
+    // 초기 타이머 설정
+    currentTimer();
+
+    // 1분마다 타이머 업데이트
+    const timerInterval = setInterval(currentTimer, 60000);
+
+    // 컴포넌트가 언마운트되면 타이머 클리어
+    return () => clearInterval(timerInterval);
+  }, []);
+
+  // 현재 시각 타이머
+  function currentTimer() {
+    const date = new Date();
+    const hour = String(date.getHours()).padStart(2, "0");
+    const min = String(date.getMinutes()).padStart(2, "0");
+    setTimer(`${hour}:${min}`);
+  }
+
   return (
     <Container>
       {menuOpen && <NavMenu />}
@@ -19,6 +44,10 @@ export default function BtmNav({
         <Image src={pixel_logo} alt="" className="pixel-logo" width={30} />
         <span>Start</span>
       </Btn>
+      <Clock>
+        <Image src={clock} alt="clock-img" width={16} />
+        <div className="timer">{timer}</div>
+      </Clock>
     </Container>
   );
 }
@@ -28,6 +57,7 @@ const Container = styled.nav`
   height: 40px;
   padding: 5px;
   display: flex;
+  justify-content: space-between;
   position: absolute;
   left: 0;
   bottom: 0;
@@ -44,10 +74,26 @@ const Btn = styled.div<BtmOpenProps>`
   gap: 5px;
   background-color: #bfbfbf;
   box-shadow: ${(props) =>
-    props.open
-      ? "rgb(71, 71, 71) 3px 3px inset, rgba(237, 237, 238) -3px -3px inset"
-      : "rgb(237, 237, 238) 3px 3px inset, rgba(71, 71, 71) -3px -3px inset"};
+    props.open ? "var(--box-shadow-black)" : "var(--box-shadow-white)"};
   font-family: "Microsoft";
   font-weight: 500;
   cursor: pointer;
+`;
+
+const Clock = styled.div`
+  height: 100%;
+  padding: 0 10px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  background-color: #bfbfbf;
+  box-shadow: var(--box-shadow-black);
+  font-family: "Microsoft";
+
+  .timer {
+    height: 16px;
+    display: flex;
+    align-items: center;
+    padding-top: 2px;
+  }
 `;
