b299400b702ff60e70582167e9cf25e4177ae9d5
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-11-10 22:46:20 +0900

    feat: add RecoilRoot

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import type { AppProps } from "next/app";
 import "@/styles/index.css";
 import "@/styles/common.css";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
