1d0d006e09ea03146fa78e877437785bcab08edd
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-11-13 09:41:22 +0900

    feat: outSideClcik 이벤트 시 bg 초기화를 위한 타입 추가

--- front/src/hooks/useChangeIconBg.tsx
+++ front/src/hooks/useChangeIconBg.tsx
@@ -4,9 +4,10 @@ export function useChangeIconBg() {
   const [changeBg, setChangeBg] = useState<undefined | number>(undefined);
   const [clicked, setClicked] = useState(false);
 
-  const changeClick = (index: number) => {
+  const changeClick = (index: number | undefined) => {
     if (!clicked) {
-      if (changeBg === index) return setChangeBg(undefined);
+      if (changeBg === index || index === undefined)
+        return setChangeBg(undefined);
       setClicked(true);
       setTimeout(() => {
         setClicked(false);
