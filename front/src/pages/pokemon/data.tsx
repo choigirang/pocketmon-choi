0c132b7f241c88ae1933f53a0d47ced7a6405ba7
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-11-16 22:49:47 +0900

    feat: 입력 키에 따른 컴포넌트 on/off

--- front/src/pages/pokemon/data.tsx
+++ front/src/pages/pokemon/data.tsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { styled } from "styled-components";
 import PartTwoStory from "../../components/story/PartTwoStory";
+import { useRecoilState } from "recoil";
+import { CharacterAtom } from "@/recoil/openAboutCharacter/characterAtom";
+import ItemOpen from "@/components/story/character/ItemOpen";
+import StatusOpen from "@/components/story/character/\bStatusOpen";
 
 export default function data() {
+  const [status, setStatus] = useRecoilState(CharacterAtom);
+
+  useEffect(() => {
+    const handleKeyboardWindow = (e: KeyboardEvent) => {
+      const key = e.key;
+      switch (key) {
+        case "i":
+          setStatus({ ...status, ITEM: !status.ITEM });
+          break;
+        case "s":
+          setStatus({ ...status, STATUS: !status.STATUS });
+          break;
+        default:
+          break;
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyboardWindow);
+
+    // 컴포넌트 언마운트 시 이벤트 리스너를 정리합니다.
+    return () => {
+      document.removeEventListener("keydown", handleKeyboardWindow);
+    };
+  }, []);
+
   return (
     <Container>
+      {status.ITEM && <ItemOpen />}
+      {status.STATUS && <StatusOpen />}
       <PartTwoStory />
     </Container>
   );
