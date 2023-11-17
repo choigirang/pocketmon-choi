74c3f8b48349eb5205edae99edb95248419b2546
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-11-17 23:55:40 +0900

    style init

--- front/src/components/story/character/ItemOpen.tsx
+++ front/src/components/story/character/ItemOpen.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { styled } from "styled-components";
 
 export default function ItemOpen() {
-  return <div>ItemOpen</div>;
+  return <Container>ItemOpen</Container>;
 }
+
+const Container = styled.div`
+  position: absolute;
+  width: 200px;
+  height: 300px;
+  z-index: 2;
+  background-color: white;
+`;
