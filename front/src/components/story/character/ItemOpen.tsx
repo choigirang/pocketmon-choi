57916ae0a26f77e00dd0b4987889f3d0575b85cc
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-11-18 19:44:42 +0900

    feat: mouse drag 방식 변경

--- front/src/components/story/character/ItemOpen.tsx
+++ front/src/components/story/character/ItemOpen.tsx
@@ -2,41 +2,38 @@ import React from "react";
 import { styled } from "styled-components";
 import useDrag from "../../../hooks/useDrag";
 
+type PosProps = {
+  pos: {
+    x: number;
+    y: number;
+  };
+};
+
 export default function ItemOpen() {
-  const {
-    containerRef,
-    dragComponentRef,
-    pos,
-    dragStartHandler,
-    dragHandler,
-    dragOverHandler,
-    dragEndHandler,
-  } = useDrag();
+  const { containerRef, movePos, dragStart } = useDrag();
 
   return (
-    <Container ref={containerRef}>
-      <Drag
-        ref={dragComponentRef}
-        draggable
-        onDragStart={dragStartHandler}
-        onDrag={dragHandler}
-        onDragOver={dragOverHandler}
-        onDragEnd={dragEndHandler}
-        style={{ left: pos.left, top: pos.top }}
-      ></Drag>
+    <Container
+      ref={containerRef}
+      onMouseDown={(e) => dragStart(e)}
+      pos={movePos}
+    >
+      <Drag></Drag>
     </Container>
   );
 }
 
-const Container = styled.div`
+const Container = styled.div<PosProps>`
+  position: absolute;
   width: 200px;
   height: 300px;
   z-index: 2;
   background-color: white;
+  left: ${(props) => props.pos.x};
+  top: ${(props) => props.pos.y};
 `;
 
 const Drag = styled.div`
-  position: absolute;
   width: 100%;
   height: 100%;
 `;
