193355de76d01dbeb9538450a387cc37dce12e9e
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-11-18 17:23:47 +0900

    feat: add drag hook

--- front/src/components/story/character/ItemOpen.tsx
+++ front/src/components/story/character/ItemOpen.tsx
@@ -1,14 +1,42 @@
 import React from "react";
 import { styled } from "styled-components";
+import useDrag from "../../../hooks/useDrag";
 
 export default function ItemOpen() {
-  return <Container>ItemOpen</Container>;
+  const {
+    containerRef,
+    dragComponentRef,
+    pos,
+    dragStartHandler,
+    dragHandler,
+    dragOverHandler,
+    dragEndHandler,
+  } = useDrag();
+
+  return (
+    <Container ref={containerRef}>
+      <Drag
+        ref={dragComponentRef}
+        draggable
+        onDragStart={dragStartHandler}
+        onDrag={dragHandler}
+        onDragOver={dragOverHandler}
+        onDragEnd={dragEndHandler}
+        style={{ left: pos.left, top: pos.top }}
+      ></Drag>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-  position: absolute;
   width: 200px;
   height: 300px;
   z-index: 2;
   background-color: white;
+`;
+
+const Drag = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
 `;
