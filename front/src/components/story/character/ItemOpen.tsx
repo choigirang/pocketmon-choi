2e3ce510d2c295c8f17f0a14900939417c1d6f14
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-11-19 13:43:40 +0900

    feat: parent size over 시 bottom, right 고정

--- front/src/components/story/character/ItemOpen.tsx
+++ front/src/components/story/character/ItemOpen.tsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "styled-components";
 import useDrag from "../../../hooks/useDrag";
 
 type PosProps = {
   pos: {
-    x: number;
-    y: number;
+    left: number;
+    top: number;
   };
 };
 
-export default function ItemOpen() {
+export default function ItemOpen({
+  size,
+}: {
+  size: React.RefObject<HTMLDivElement>;
+}) {
+  const [checkOver, setCheckOver] = useState({
+    right: false,
+    bottom: false,
+  });
   const { containerRef, movePos, dragStart } = useDrag();
+
+  useEffect(() => {
+    // right Over check
+    const childRightPos = containerRef.current!.getBoundingClientRect().right;
+    const parentRightPos = size.current!.getBoundingClientRect().right;
+
+    // bottom Over check
+    const childBtmPos = containerRef.current!.getBoundingClientRect().bottom;
+    const parentBtmPos = size.current!.getBoundingClientRect().bottom;
+
+    if (childBtmPos > parentBtmPos) {
+      setCheckOver((prev) => ({ ...prev, bottom: true }));
+    } else {
+      setCheckOver((prev) => ({ ...prev, bottom: false }));
+    }
+
+    if (childRightPos > parentRightPos) {
+      setCheckOver((prev) => ({ ...prev, right: true }));
+    } else {
+      setCheckOver((prev) => ({ ...prev, right: false }));
+    }
+  }, [movePos]);
 
   return (
     <Container
@@ -29,8 +59,8 @@ const Container = styled.div<PosProps>`
   height: 300px;
   z-index: 2;
   background-color: white;
-  left: ${(props) => props.pos.x};
-  top: ${(props) => props.pos.y};
+  left: ${(props) => `${props.pos.left}px`};
+  top: ${(props) => `${props.pos.top}px`};
 `;
 
 const Drag = styled.div`
