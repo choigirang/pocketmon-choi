a35ea97dfe0b86595e7cf6d76585672d98b691dc
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-11-19 19:48:06 +0900

    feat: parent size over 시 조건부 left, top 위치 설정

--- front/src/hooks/useDrag.tsx
+++ front/src/hooks/useDrag.tsx
@@ -4,12 +4,15 @@ export default function useDrag(size: React.RefObject<HTMLDivElement>) {
   const [startPos, setStartPos] = useState({ x: 0, y: 0 });
   const [movePos, setMovePos] = useState({
     left: 0,
+    right: 0,
     top: 0,
-    right: false,
-    bottom: false,
+    bottom: 0,
   });
   const [isDragging, setIsDragging] = useState(false);
   const containerRef = useRef<HTMLDivElement>(null);
+
+  const checkChildRefSize = containerRef.current?.getBoundingClientRect();
+  const checkParentRefSize = size.current?.getBoundingClientRect();
 
   const dragStart = (e: React.MouseEvent) => {
     setIsDragging(true);
@@ -24,7 +27,40 @@ export default function useDrag(size: React.RefObject<HTMLDivElement>) {
       if (containerRef.current) {
         const left = e.clientX - startPos.x;
         const top = e.clientY - startPos.y;
-        setMovePos((prev) => ({ ...prev, left, top }));
+
+        console.log(left, top);
+        setMovePos((prev) => ({
+          ...prev,
+          left: prev.left + left,
+          top: prev.top + top,
+        }));
+
+        // // parent size over
+        // if (!checkChildRefSize || !checkParentRefSize) return;
+
+        // // parent bottom size over
+        // if (checkChildRefSize.bottom < checkParentRefSize.bottom) {
+        //   setMovePos((prev) => ({ ...prev, bottom: 0 }));
+        // } else {
+        //   setMovePos((prev) => ({ ...prev, bottom: 1 }));
+        // }
+
+        // // parent right size over
+        // if (checkChildRefSize.right < checkParentRefSize.right) {
+        //   setMovePos((prev) => ({ ...prev, top: 0 }));
+        // } else {
+        //   setMovePos((prev) => ({ ...prev, top: 1 }));
+        // }
+
+        // // parent top size over
+        // if (checkChildRefSize.top > checkParentRefSize.top) {
+        //   setMovePos((prev) => ({ ...prev, top: 0 }));
+        // }
+
+        // // parent left size over
+        // if (checkChildRefSize.left > checkParentRefSize.left) {
+        //   setMovePos((prev) => ({ ...prev, left: 0 }));
+        // }
       }
     };
 
