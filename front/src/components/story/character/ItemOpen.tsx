b0fa442c21aa55981b585ffdfc69fe4699b1b31a
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-11-20 10:00:25 +0900

    chore: change event 부여 방식
    useEffect로 사용하는 event와 직접 부여되는 함수 이벤트의 차이점을 파악해보아야 함

--- front/src/components/story/character/ItemOpen.tsx
+++ front/src/components/story/character/ItemOpen.tsx
@@ -10,11 +10,10 @@ export default function ItemOpen({
 }) {
   const { containerRef, movePos, dragStart } = useDrag(size);
 
-  console.log(movePos);
   return (
     <Container
       ref={containerRef}
-      onMouseDown={(e) => dragStart(e)}
+      onMouseDown={dragStart}
       $pos={movePos}
     ></Container>
   );
@@ -28,22 +27,4 @@ const Container = styled.div<PosProps>`
   background-color: white;
   left: ${(props) => props.$pos.left + "px"};
   top: ${(props) => props.$pos.top + "px"};
-  /* ${(props) => {
-    const pos = props.$pos;
-
-    // parent bottom size over
-    if (pos.bottom) {
-      console.log("css parent bottom over");
-
-      return `left: ${pos.left}px; bottom: 0`;
-    }
-
-    // parent right size over
-    if (pos.right) {
-      console.log("css parent right over");
-      return `top: ${pos.top}px; right: 0px`;
-    }
-
-    return `left: ${pos.left}px; top: ${pos.top}px`;
-  }} */
 `;
