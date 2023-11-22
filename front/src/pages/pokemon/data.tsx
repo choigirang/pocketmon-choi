62fdb0700a54d02a3dea453ec4e7f89cfec43e65
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-11-22 19:13:09 +0900

    feat: focus tabIndex를 위한 키보드 이벤트 한 번에 처리 (+ change recoilState) 4348546

--- front/src/pages/pokemon/data.tsx
+++ front/src/pages/pokemon/data.tsx
@@ -1,11 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { styled } from "styled-components";
 import PartTwoStory from "../../components/story/PartTwoStory";
+import useMoveClass from "@/hooks/useMoveClass";
 
 export default function data() {
+  const focusRef = useRef<HTMLDivElement>(null);
+
+  const { status, moveClass, character, handleKey } = useMoveClass();
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      focusRef.current?.focus();
+    }
+  }, []);
+
   return (
-    <Container>
-      <PartTwoStory />
+    <Container onKeyDown={handleKey} ref={focusRef} tabIndex={0}>
+      <PartTwoStory
+        status={status}
+        moveClass={moveClass}
+        character={character}
+      />
     </Container>
   );
 }
