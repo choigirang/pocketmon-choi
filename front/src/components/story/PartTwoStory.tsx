4b58a4b8b6a0fbefd168e89a77c1fac55f60b783
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-11-22 19:11:12 +0900

    feat: focus를 위해 키보드 핸들러를 상위 페이지 index로 이동
    상위 페이지 div에서 keyboard 입력에 대한 class명 변경과 ItemOpen, StatusOpen 입력 한 번에 처리

--- front/src/components/story/PartTwoStory.tsx
+++ front/src/components/story/PartTwoStory.tsx
@@ -4,54 +4,19 @@ import React, { useEffect, useRef, useState } from "react";
 import { useRecoilState } from "recoil";
 import { styled } from "styled-components";
 import ItemOpen from "./character/ItemOpen";
-import { CharacterPos } from "@/types/props";
+import { CharacterPos, dataPageProps } from "@/types/props";
 
-export default function PartTwoStory() {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const characterRef = useRef<HTMLDivElement>(null);
-
-  const { moveClass, character, handleMove } = useMoveClass();
-
-  useEffect(() => {
-    characterRef.current?.focus();
-  }, []);
-
-  const [status, setStatus] = useRecoilState(CharacterAtom);
-
-  useEffect(() => {
-    const handleKeyboardWindow = (e: KeyboardEvent) => {
-      const key = e.key;
-      switch (key) {
-        case "i":
-          setStatus({ ...status, ITEM: !status.ITEM });
-          console.log(status);
-          break;
-        case "s":
-          setStatus({ ...status, STATUS: !status.STATUS });
-          break;
-        default:
-          break;
-      }
-    };
-
-    document.addEventListener("keydown", handleKeyboardWindow);
-
-    return () => {
-      document.removeEventListener("keydown", handleKeyboardWindow);
-    };
-  }, []);
+export default function PartTwoStory({
+  status,
+  character,
+  moveClass,
+}: dataPageProps) {
+  const parentRef = useRef<HTMLDivElement>(null);
 
   return (
-    <Bg ref={containerRef}>
-      <Character
-        tabIndex={0}
-        onKeyDown={handleMove}
-        left={character.x}
-        top={character.y}
-        ref={characterRef}
-        className={moveClass}
-      />
-      {status.ITEM && <ItemOpen size={containerRef} />}
+    <Bg ref={parentRef}>
+      <Character $left={character.x} $top={character.y} className={moveClass} />
+      {status.ITEM && <ItemOpen parentRef={parentRef} />}
     </Bg>
   );
 }
@@ -65,6 +30,6 @@ const Bg = styled.div`
 
 const Character = styled.div<CharacterPos>`
   position: absolute;
-  left: ${(props) => `${props.left}px`};
-  top: ${(props) => `${props.top}px`};
+  left: ${(props) => `${props.$left}px`};
+  top: ${(props) => `${props.$top}px`};
 `;
