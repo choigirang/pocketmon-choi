fb7422ceebf876b55bd6a00694c118822da647b7
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-11-25 20:09:47 +0900

    feat: add useCallback

--- front/src/hooks/useHandleDataPage.tsx
+++ front/src/hooks/useHandleDataPage.tsx
@@ -1,6 +1,6 @@
 import { StrScript } from "@/constant/constant";
 import { CharacterAtom } from "@/recoil/openAboutCharacter/characterAtom";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 
 type MoveDirection = {
@@ -25,69 +25,77 @@ export default function useHandleDataPage() {
   };
 
   // 캐릭터 이동
-  const arrowMove = (key: string) => {
-    switch (key) {
-      case "ArrowUp":
-        setMoveClass(moveClass !== "bg-back" ? "bg-back" : "bg-back1");
-        if (character.x >= 0 && character.x <= 352 && character.y === 96) {
-          return;
-        }
-        if (character.x >= 384 && character.y === 32) {
-          return;
-        }
-        moveCharacter({ y: character.y - 32 });
-        break;
-      case "ArrowDown":
-        setMoveClass(moveClass !== "bg-front" ? "bg-front" : "bg-front1");
-        if (character.y >= 512) return;
-        moveCharacter({ y: character.y + 32 });
-        break;
-      case "ArrowLeft":
-        setMoveClass(moveClass !== "bg-left" ? "bg-left" : "bg-left1");
-        if (character.x === 0) return;
-        if (character.x === 384 && character.y < 96) return;
-        moveCharacter({ x: character.x - 32 });
-        break;
-      case "ArrowRight":
-        setMoveClass(moveClass !== "bg-right" ? "bg-right" : "bg-right1");
-        if (character.x === 736) return;
-        moveCharacter({ x: character.x + 32 });
-        break;
-      default:
-        break;
-    }
-  };
+  const arrowMove = useCallback(
+    (key: string) => {
+      console.log("arrowmove");
+      switch (key) {
+        case "ArrowUp":
+          setMoveClass(moveClass !== "bg-back" ? "bg-back" : "bg-back1");
+          if (character.x >= 0 && character.x <= 352 && character.y === 96) {
+            return;
+          }
+          if (character.x >= 384 && character.y === 32) {
+            return;
+          }
+          moveCharacter({ y: character.y - 32 });
+          break;
+        case "ArrowDown":
+          setMoveClass(moveClass !== "bg-front" ? "bg-front" : "bg-front1");
+          if (character.y >= 512) return;
+          moveCharacter({ y: character.y + 32 });
+          break;
+        case "ArrowLeft":
+          setMoveClass(moveClass !== "bg-left" ? "bg-left" : "bg-left1");
+          if (character.x === 0) return;
+          if (character.x === 384 && character.y < 96) return;
+          moveCharacter({ x: character.x - 32 });
+          break;
+        case "ArrowRight":
+          setMoveClass(moveClass !== "bg-right" ? "bg-right" : "bg-right1");
+          if (character.x === 736) return;
+          moveCharacter({ x: character.x + 32 });
+          break;
+        default:
+          break;
+      }
+    },
+    [character]
+  );
 
   // 아이템창 & 스탯창
-  const handleKey = (e: React.KeyboardEvent<HTMLDivElement>) => {
-    const { key } = e;
+  const handleKey = useCallback(
+    (e: React.KeyboardEvent<HTMLDivElement>) => {
+      const { key } = e;
 
-    if (!status.ITEM && !status.STATUS) arrowMove(key);
+      if (!status.ITEM && !status.STATUS) arrowMove(key);
 
-    switch (key) {
-      case "i":
-        setStatus((prev) => ({ ...prev, ITEM: !prev.ITEM }));
-        break;
-      case "s":
-        setStatus((prev) => ({ ...prev, STATUS: !prev.STATUS }));
-        break;
-      default:
-        break;
-    }
-  };
+      switch (key) {
+        case "i":
+          setStatus((prev) => ({ ...prev, ITEM: !prev.ITEM }));
+          break;
+        case "s":
+          setStatus((prev) => ({ ...prev, STATUS: !prev.STATUS }));
+          break;
+        default:
+          break;
+      }
+    },
+    [status, arrowMove]
+  );
 
   // 아이템창/스탯 선택
-  const handleArrow = (
-    DATA: StrScript,
-    e: React.KeyboardEvent<HTMLDivElement>
-  ) => {
-    const ObjectLength = Object.keys(DATA).length;
-    if (e.key === "ArrowDown" && selectNum !== ObjectLength - 1) {
-      setSelectNum((prev) => prev + 1);
-    }
-    if (e.key === "ArrowUp" && selectNum !== 0)
-      setSelectNum((prev) => prev - 1);
-  };
+  const handleArrow = useCallback(
+    (DATA: StrScript, e: React.KeyboardEvent<HTMLDivElement>) => {
+      const ObjectLength = Object.keys(DATA).length;
+      if (e.key === "ArrowDown" && selectNum !== ObjectLength - 1) {
+        setSelectNum((prev) => prev + 1);
+      }
+      if (e.key === "ArrowUp" && selectNum !== 0) {
+        setSelectNum((prev) => prev - 1);
+      }
+    },
+    [selectNum]
+  );
 
   return {
     status,
