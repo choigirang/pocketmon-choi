5dbf1d8fffbab01fe1076565412f01df63d570f2
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-13 22:03:46 +0900

    add head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,21 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import { styled } from "styled-components";
 import WindowWallPaper from "@/components/desktop/WindowWallPaper";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <Container>
-      <WindowWallPaper></WindowWallPaper>
-    </Container>
+    <React.Fragment>
+      <Head>
+        <title>포켓몬최 키우기</title>
+        <meta name="description" content="포켓몬 골드"></meta>
+      </Head>
+      <Container>
+        <WindowWallPaper></WindowWallPaper>
+      </Container>
+    </React.Fragment>
   );
 }
 
