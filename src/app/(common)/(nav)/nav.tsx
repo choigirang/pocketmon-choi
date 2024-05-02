78fa720274ab5b42a7f9a3aa29e990e28cef7fa0
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-02 15:35:52 +0900

    refactor : Nav

--- src/app/(common)/(nav)/nav.tsx
+++ src/app/(common)/(nav)/nav.tsx
@@ -1,3 +1,11 @@
-export default function BottomNav() {
-  return <nav></nav>;
+import Menu from "./menu";
+import Timer from "./timer";
+
+export default function Nav() {
+  return (
+    <nav className="fixed bottom-0 left-0 flex justify-between w-full h-[40px] bg-gray shadow-nav p-1">
+      <Menu />
+      <Timer />
+    </nav>
+  );
 }
