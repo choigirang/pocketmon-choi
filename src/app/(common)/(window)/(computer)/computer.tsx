860c883ea7d1b11cd9c657160dc2d368e1f144ca
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-04 23:14:47 +0900

    refactor: Computer folder => (list)
    error : before initialization 해결 필요
    IconImg 안에서 Computer 를 가져오고 다시 Computer folder 에서 iconImg import

---
+++ src/app/(common)/(window)/(computer)/computer.tsx
@@ -0,0 +1,18 @@
+import IconContainer from "@/app/(home)/IconContainer";
+import IconImg from "@/app/(home)/iconImg";
+import { IconData } from "@/constant/constant";
+
+export default function Computer() {
+  return (
+    <div className="w-full h-full bg-white shadow-inner">
+      {Object.keys(IconData).map((data) => (
+        <IconImg
+          key={data}
+          name={data}
+          url={`/image/${data}.webp`}
+          components={IconData[data].components}
+        />
+      ))}
+    </div>
+  );
+}
