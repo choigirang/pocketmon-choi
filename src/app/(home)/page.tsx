4a3da893ea020166e57b480aecbd5b7116846eba
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-04 23:18:45 +0900

    refactor: Home page
    아이콘 목록

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-  return <div></div>;
+import { IconData } from "@/constant/constant";
+import Image from "next/image";
+import IconContainer from "./IconContainer";
+
+export default function Home() {
+  return (
+    <div className="w-full h-full p-3">
+      <IconContainer />
+      <Image
+        src="/image/windows_logo.webp"
+        alt="logo"
+        width={500}
+        height={300}
+        priority
+        className="h-auto absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-[1]"
+      />
+    </div>
+  );
 }
