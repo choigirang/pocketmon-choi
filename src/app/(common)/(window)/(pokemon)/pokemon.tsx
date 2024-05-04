57aa688a73215ec26b48bd91b351b24c6c69bd41
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-04 23:15:20 +0900

    refactor: open Pokemon window

---
+++ src/app/(common)/(window)/(pokemon)/pokemon.tsx
@@ -0,0 +1,9 @@
+import Loading from "./loading";
+
+export default function Pokemon() {
+  return (
+    <div className="w-full h-full bg-black relative shadow-common">
+      <Loading />
+    </div>
+  );
+}
