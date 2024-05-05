5b1442fe22bf76f83e4ec6e1f0d6bd9761043f94
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-05 22:37:57 +0900

    delete IconContainer

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,30 @@
 import { IconData } from "@/constant/constant";
 import Image from "next/image";
-import IconContainer from "./IconContainer";
+import IconImg from "./iconImg";
 
 export default function Home() {
   return (
-    <div className="w-full h-full p-3">
-      <IconContainer />
+    <main className="w-full h-full p-3 grid grid-cols-wallpaper grid-rows-wallpaper gap-3">
+      <ul>
+        {Object.keys(IconData).map((data) => (
+          <IconImg
+            key={data}
+            name={data}
+            url={`/image/${data}.webp`}
+            components={IconData[data].components}
+            menu={false}
+          />
+        ))}
+      </ul>
       <Image
         src="/image/windows_logo.webp"
         alt="logo"
         width={500}
         height={300}
         priority
-        className="h-auto absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-[1]"
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-[1]"
+        style={{ width: "auto", height: "auto" }}
       />
-    </div>
+    </main>
   );
 }
