0609f22118c265a7e1e65451faa02e18149091ef
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-05 22:34:49 +0900

    refactor: loading 종료 후 다음 챕터

--- src/app/(common)/(window)/(pokemon)/loading.tsx
+++ src/app/(common)/(window)/(pokemon)/loading.tsx
@@ -3,14 +3,15 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
-export default function Loading() {
+export default function Loading({ setStep }: { setStep: () => void }) {
   const [loading, setLoading] = useState(true);
 
   // 초기 배경 setTime
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 5000);
+      setStep();
+    }, 3000);
 
     return () => {
       clearTimeout(timer);
@@ -20,13 +21,13 @@ export default function Loading() {
   return (
     <React.Fragment>
       {loading && (
-        <Image
-          src="/image/loading.png"
-          fill
-          objectFit="cover"
-          alt="loading"
-          sizes="100vw"
-        />
+        <div className="w-full h-full relative">
+          <Image src="/image/loading.png" fill alt="loading" sizes="100vw" />
+
+          <span className="absolute z-100 top-[50px] left-1/2 -translate-x-1/2 text-[#ffcc01] text-2xl font-pokemon text-logo">
+            Pokemon Choi
+          </span>
+        </div>
       )}
     </React.Fragment>
   );
