f237c4954979f2c63104e7d8d2e3ef5fe33338f2
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-06 23:57:21 +0900

    refactor: openWindow, iconImg 내부 => 바탕화면

---
+++ src/app/(common)/(window)/(frame)/openWindow.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { windowState } from "@/recoil/window/atom";
+import WindowFrame from "./windowFrame";
+import { useRecoilValue } from "recoil";
+import React from "react";
+
+/* 2024/05/06 - 바탕화면(home/page)에서 보여줄 윈도우 프로그램 열기*/
+export default function OpenWindow() {
+  const lists = useRecoilValue(windowState);
+
+  return (
+    <React.Fragment>
+      {lists.map((list) => {
+        return (
+          list.open && (
+            <WindowFrame key={list.name} props={list}>
+              {list.components}
+            </WindowFrame>
+          )
+        );
+      })}
+    </React.Fragment>
+  );
+}
