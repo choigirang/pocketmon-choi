ea49c3c91c7b1d2af815c8b68eb5ea24b03159cb
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-06 23:55:29 +0900

    refactor: menu btn, 열린 프로그램 목록 추가

--- src/app/(common)/(nav)/menu.tsx
+++ src/app/(common)/(nav)/menu.tsx
@@ -4,44 +4,52 @@ import IconImg from "@/app/(home)/iconImg";
 import { IconData } from "@/constant/constant";
 import Image from "next/image";
 import React, { useState } from "react";
+import Computer from "../(window)/(computer)/computer";
 
+/** 2024/05/06 - 시작 메뉴, 프로그램 목록*/
 export default function Menu() {
   const [openMenu, setOpenMenu] = useState(false);
 
   return (
-    <React.Fragment>
-      <div
-        className={`flex items-center gap-2 px-[5px] cursor-pointer ${openMenu ? "shadow-timer" : "shadow-menu"} transition-custom`}
-        onClick={() => setOpenMenu((prev) => !prev)}
-      >
-        <Image
-          src="/image/window_pixel.webp"
-          alt="menu icon"
-          width={24}
-          height={24}
-        />
-        <span>Start</span>
-      </div>
+    <div
+      className={`flex items-center gap-2 px-[5px] cursor-pointer ${openMenu ? "shadow-timer" : "shadow-menu"} transition-custom`}
+      onClick={() => setOpenMenu((prev) => !prev)}
+    >
+      {/* menu button */}
+      <Image
+        src="/image/window_pixel.webp"
+        alt="menu icon"
+        width={24}
+        height={24}
+      />
+      <span>Start</span>
+      {/* menu list */}
       {openMenu && (
-        <ul className="absolute bottom-[40px] flex bg-gray shadow-menu">
-          <li className="writing-mode w-5 h-[200px] py-[10px] bg-[#7f8279] font-bold">
+        <ul className="absolute left-0 bottom-[40px] flex bg-gray shadow-menu">
+          <li className="flex items-center writing-mode w-7 min-h-[200px] py-[10px] bg-[#7f8279] font-bold">
             Window 95
           </li>
           <li className="h-full">
-            <ul>
+            <ul className="flex flex-col">
               {Object.keys(IconData).map((data) => (
                 <IconImg
                   key={data}
                   name={data}
-                  url={`/image/${data}.webp`}
                   components={IconData[data].components}
-                  menu={true}
+                  menu
                 />
               ))}
+              {/* 컴퓨터 안에 컴퓨터 호출 초기화 문제로 별도 추가 */}
+              <IconImg
+                key={"computer"}
+                name={"computer"}
+                components={Computer()}
+                menu
+              />
             </ul>
           </li>
         </ul>
       )}
-    </React.Fragment>
+    </div>
   );
 }
