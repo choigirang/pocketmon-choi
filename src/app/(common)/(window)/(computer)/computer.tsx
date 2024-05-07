d83029afeb98a8465f626fc9d47761f86851058a
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-08 01:42:43 +0900

    feat: 바탕화면, 유저가 추가한 파일 목록

--- src/app/(common)/(window)/(computer)/computer.tsx
+++ src/app/(common)/(window)/(computer)/computer.tsx
@@ -1,10 +1,11 @@
 import IconImg from "@/app/(home)/iconImg";
 import { IconData } from "@/constant/constant";
+import UserFiles from "@/app/(home)/userFiles";
 
 /** 2024/05/06 - 컴퓨터 폴더 안에 아이콘 목록 */
 export default function Computer() {
   return (
-    <div className="w-full h-full overflow-scroll p-3 grid grid-rows-4 grid-cols-7 bg-white shadow-inner">
+    <ul className="w-full h-full overflow-scroll p-3 grid grid-rows-4 grid-cols-7 bg-white shadow-inner">
       {Object.keys(IconData).map((data) => (
         <IconImg
           key={data}
@@ -13,6 +14,7 @@ export default function Computer() {
           folder
         />
       ))}
-    </div>
+      <UserFiles folder />
+    </ul>
   );
 }
