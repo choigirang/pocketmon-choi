ced6a56efceefaba5ed4388cc064631d5623befa
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-08 01:43:27 +0900

    feat: window button func => hooks

--- src/app/(common)/(window)/(frame)/buttons.tsx.tsx
+++ src/app/(common)/(window)/(frame)/buttons.tsx.tsx
@@ -1,36 +1,22 @@
 "use client";
 
-import { WindowItem, windowState } from "@/recoil/window/atom";
-import { useRecoilState } from "recoil";
+import useWindowAtom from "@/hooks/useWindowAtom";
+import { WindowItem } from "@/recoil/window/windowState";
 
 const handleButton = ["_", "x"];
 
 export default function Buttons(props: WindowItem) {
-  const [isWindow, setIsWindow] = useRecoilState(windowState);
-
-  /* atom의 윈도우 목록에서 창 닫기 */
-  const handleCloseButtonClick = (func: string) => {
-    if (func === "_") {
-      setIsWindow((prev) =>
-        prev.map((item) =>
-          item.name === props.name ? { ...item, open: false } : item
-        )
-      );
-    }
-    if (func === "x")
-      setIsWindow((prev) => prev.filter((item) => item.name !== props.name));
-  };
-
+  const { handleOnOff } = useWindowAtom(props);
   return (
     <div className="flex gap-1">
       {handleButton.map((func) => (
         <button
           key={func}
           type="button"
-          className="w-[15px] h-full text-sm bg-gray text-zinc-800 shadow-common"
-          onClick={() => handleCloseButtonClick(func)}
+          className="flex justify-center w-[15px] h-full text-sm bg-gray text-zinc-800 shadow-common px-2 py-1 text-center"
+          onClick={() => handleOnOff(func)}
         >
-          {func}
+          <span>{func}</span>
         </button>
       ))}
     </div>
